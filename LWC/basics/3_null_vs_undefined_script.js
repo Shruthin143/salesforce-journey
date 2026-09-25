9f0e5180d283f2f9b31a6a6f71c6e71f89095a59
Shruthin143 <pasunoori.1@alumni.iitj.ac.in>
2026-09-25 14:33:01 +0530

    nullVSundef commit

---
+++ LWC/basics/3_null_vs_undefined_script.js
@@ -0,0 +1,17 @@
+//Equality Comprison
+
+//Normal comparison(==), compares only the value and not the datatype
+console.log(3 == '3')
+console.log('shr' == 'shr')
+
+//Strict comparison (===), compares both value and the datatype
+console.log(3 === '3')
+console.log('shr' === 'shr')
+
+//null vs undefined
+let x
+console.log(typeof x)
+let y = null
+console.log(typeof y)
+console.log(x == y)
+console.log(x === y)
