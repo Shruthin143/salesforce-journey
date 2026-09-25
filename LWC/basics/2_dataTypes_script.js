01daa93a2ddb745ca6b02a38af847514a466c6c2
Shruthin143 <pasunoori.1@alumni.iitj.ac.in>
2026-09-25 14:19:36 +0530

    DataTypes Commit

---
+++ LWC/basics/2_dataTypes_script.js
@@ -0,0 +1,39 @@
+/*
+Number
+string
+boolean
+bigInt
+null
+object
+undefined
+symbol
+
+Rest all like Arry, Date, Math, String etc are in called Object in some form
+*/
+
+let num = 35
+console.log(typeof num)
+
+let str = 'shr'
+console.log(typeof str)
+
+let bool = true
+console.log(typeof bool)
+
+let bigNum = 10n
+console.log(typeof bigNum)
+
+let undef
+console.log(typeof undef)
+
+let nullValue = null
+console.log(typeof nullValue)
+
+let obj = {}
+console.log(typeof obj)
+
+let arr = []
+console.log(typeof arr)
+
+let sym = Symbol('id')
+console.log(typeof sym)
