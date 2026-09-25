f5bb441af9a114d184078829d123ab2423bd72b1
Shruthin143 <pasunoori.1@alumni.iitj.ac.in>
2026-09-25 16:24:50 +0530

    5 Destructuring

---
+++ LWC/basics/5_destructuring_script.js
@@ -0,0 +1,8 @@
+//Arrau destructing
+let arr = ['amazon', 'google', 'facebook']
+let [a, , c] = arr
+console.log(a, c)
+
+let obj = { name: 'shruthin', age: 34 }
+let { name: n, age: b } = obj
+console.log(n, b)
